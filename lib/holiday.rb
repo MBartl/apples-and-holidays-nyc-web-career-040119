d94bea494781b3bc8827f782dd93a90b79a8f2cc
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 01:24:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,15 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = []
+  holiday_hash[:winter].each do |holiday, supplies|
+    i = 0
+    for i < supplies.length do
+      winter_supplies.push(supplies[i])
+      i += 0
+    end
+  end
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -63,10 +71,4 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
 
