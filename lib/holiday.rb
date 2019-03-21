09bd0aab65634235ec4a98327d8f0c1f2e1b0836
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 02:30:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_name.capitalize!
     output.push("#{season_name}:")
     season.each do |holiday, supplies|
-      puts "#{holiday, supplies}"
+      puts "#{holiday}: #{supplies}}"
     end
   end
   output
