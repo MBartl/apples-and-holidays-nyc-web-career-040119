b338bfa68a6565ee16bb474490a00398491ab308
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 03:15:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -102,7 +102,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holiday_hash[season].each do |holiday, supplies|
       if supplies.include? ("BBQ")
-        bbq_days.push(season)
+        bbq_days.push(holiday)
       end
     end
   end
